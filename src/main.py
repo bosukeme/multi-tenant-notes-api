32dded93fc5b539061faae49ef8ae9110349f41e
bosukeme <ukeme.wilson@gmail.com>
2025-10-26 18:41:49 +0100

    added cors loggin and rate limiting middlewares

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from src.db.connection import init_db
+from src.middlewares import register_middleware
 from src.organizations.routes import org_router
 from src.users.routes import user_router
 from src.notes.routes import note_router
+from src.middlewares.rate_limit import apply_rate_limit_to_router
 
 
 @asynccontextmanager
@@ -18,10 +20,15 @@ async def life_span(app: FastAPI):
 
 def register_routers(app: FastAPI) -> None:
 
+    apply_rate_limit_to_router(org_router, "5/minute")
     app.include_router(org_router, prefix="/organizations",
                        tags=["Organizations"])
+
+    apply_rate_limit_to_router(user_router, "5/minute")
     app.include_router(user_router, prefix="/organizations/{org_id}/users",
                        tags=["users"])
+
+    apply_rate_limit_to_router(note_router, "5/minute")
     app.include_router(note_router, prefix="/notes",
                        tags=["notes"])
 
@@ -33,6 +40,11 @@ def create_app() -> FastAPI:
         lifespan=life_span
     )
 
+    @app.get("/")
+    def home():
+        return "Welcome to the Multi Tenant Notes API"
+
+    register_middleware(app)
     register_routers(app)
 
     return app
